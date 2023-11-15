be692e5145acece098bad80987a3046923252c60
Ana <anarosca66@gmail.com>
2023-11-15 09:07:40 +0200

    css index

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,31 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <title>Listă cărți</title>
+    <style>
+        body {
+        
+}
+
+    </style>
 </head>
 <body>
-    <h1>Listă cărți</h1>
-    <a href="/books/create">Adaugă carte nouă</a>
+    <h1 style="display:flex;justify-content:center;">Listă cărți</h1>
+    <a style="display:flex;justify-content:right;text-decoration:none;color:black;" href="/books/create">Adaugă carte nouă</a>
 
-    <table>
+    <table  style="border:1px solid black;margin-left:200px;">
         <thead>
-            <tr>
-                <th>Titlu</th>
-                <th>Autor</th>
-                <!-- Alte antete pentru informații suplimentare -->
-            </tr>
+            <tr >
+                <th style="padding:0  350px 0 80px;">Titlu</th>
+                <th >Autor</th>
+                
         </thead>
         <tbody>
             @foreach($books as $book)
             <tr>
-                <td>{{ $book->title }}</td>
+                <td style="padding:0  250px 0 80px;">{{ $book->title }}</td>
                 <td>{{ $book->author }}</td>
-              
+              <td style="padding:0  250px 0 80px; display:flex;justify-content:row;">
+              <button style="margin:0 10px 0 10px;"><a style="text-decoration:none;color:black;" href="/books/{{ $book->id }}/edit">Editează</a></button>
                 <form action="/books/{{ $book->id }}" method="POST">
                         @csrf
                         @method('DELETE')
                         <button type="submit">Șterge</button>
                     </form>
-                    <a href="/books/{{ $book->id }}/edit">Editează</a>
+                    </td>    
             </tr>
             @endforeach
         </tbody>
